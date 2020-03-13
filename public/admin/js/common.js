769eacdfd716c90b0342c70d63cf0e746eaa4fbd
dongc <1575901630@qq.com>
2020-03-13 15:26:32 +0800

    首页基础样式完成

---
+++ public/admin/js/common.js
@@ -0,0 +1,52 @@
+$(function () {
+
+    //修复页面一刷新出现在padding动画 因为给body设置的过渡到是body出现padding的时候出现了动画效果
+    //解决办法等到页面加载完成执行js的时候在加上过渡动画
+
+    $('body').attr('transition', 'padding .6s')
+
+    //ajax进度条
+    //监听第一个ajax请求
+    $(document).ajaxStart(function () {
+        //进度条开始移动
+        NProgress.start();
+    })
+
+    //监听最后一个ajax请求发送完成
+    $(document).ajaxStop(function () {
+        //进度条直接结束
+        NProgress.done();
+
+    })
+    // 配置禁用小圆环
+    NProgress.configure({ showSpinner: false });
+
+    //点击显示隐藏二级菜单
+
+    $('.flei').click(function () {
+        $('.in').stop().slideToggle();
+    })
+
+    //点击显示隐藏侧边栏
+    $('.move').click(function (e) {
+        //阻止a标签的默认行为
+        e.preventDefault();
+        // console.log(1);
+
+        $('.aside').toggleClass('t_left');
+        $('body').toggleClass('pl_180');
+        $('.lt-header').toggleClass('pl_180');
+    })
+
+    //点击退出按钮返回登录界面
+    $('.btn-ok').click(function () {
+        $.ajax({
+            type: 'get',
+            url: '/employee/employeeLogout',
+            dataType: 'json',
+            success: function (res) {
+                res.success && (location.href = 'http://localhost:3000/admin/login.html')
+            }
+        })
+    })
+})
--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,126 +1,99 @@
 //入口函数
 $(function () {
-    //调用bootstrap-validator插件 表单校验插件 依赖于bootstrap使用的都市bs风格的样式
-    //使用表单校检插件
+    //使用表单校验插件
     $('.form-login').bootstrapValidator({
-        //默认值 不设置默认就是指定这个属性的表单元素不进行校验
         //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
         excluded: [':disabled', ':hidden', ':not(:visible)'],
 
-        // feedbackIcons这个属性指定校检的图标显示默认是bt风格
+        //2. 指定校验时的图标显示，默认是bootstrap风格
         feedbackIcons: {
-            //成功时显示的图片
-            valid: 'glyphicon glyphicon-ok-circle',
-            //失败时显示的图片
-            invalid: 'glyphicon glyphicon-remove-circle',
-            //确认是显示的图片 都是bootstrap的字体图标 鼠标点击确认时候显示
+            //c成功时
+            valid: 'glyphicon glyphicon-ok',
+            // 失败时
+            invalid: 'glyphicon glyphicon-remove',
+            //确认时
             validating: 'glyphicon glyphicon-refresh'
         },
 
-        //指定校检的字段(数据)
+        //3. 指定校验字段
         fields: {
-            //校检用户名
+            //校验用户名，对应name表单的name属性
             username: {
-                //指定校检的方式
                 validators: {
-                    //非空校检
+                    //不能为空
                     notEmpty: {
-                        //如果用户名为空提示的数据
                         message: '用户名不能为空'
                     },
-
-                    //长度检验
+                    //长度校验
                     stringLength: {
-                        //最小长度
-                        min: 3,
+                        min: 2,
                         max: 6,
-                        //校检不通过提示的数据
-                        message: '用户名长度必须在3--6之间'
+                        message: '用户名长度必须在2到6之间'
                     },
-                    //添加提示信息 每个校检只能添加一次
                     callback: {
-                        message: '用户名不存在'
+                        message: '用户名不存在!'
                     }
                 }
             },
-            //校检密码
+
+
+
+            //校验密码，对应passwrod表单的name属性
             password: {
-                //指定校检的方式
                 validators: {
-                    //非空校检
+                    //不能为空
                     notEmpty: {
-                        //如果用户名为空提示的数据
                         message: '密码不能为空'
                     },
-
-                    //长度检验
+                    //长度校验
                     stringLength: {
-                        //最小长度
                         min: 6,
                         max: 12,
-                        //校检不通过提示的数据
-                        message: '密码长度必须在6--12之间'
+                        message: '密码长度必须在6到12之间'
                     },
-                    //添加提示信息 每个校检只能添加一次
                     callback: {
                         message: '密码错误!'
                     }
                 }
+
             }
+
         }
-    })
 
 
-
-    //重置功能
-    //点击重置按钮恢复默认样式
-    $('.btn-reset').click(function () {
-        //bootstrapValidator提供了重置表单的功能但使用之前需要先获取表单校检实例
-        //固定语法 $(表单).data('bootstrapValidator')返回的结果就是表单校检实例
-        // $(表单).data('bootstrapValidator').resetFrom()可以重置表单的样式隐藏所有的提示和图标
-        $('.form-login').data('bootstrapValidator').resetForm()
     })
 
 
 
 
-    //登录跳转功能
-    //bootstrapValidator提供了一个事件当表单校检成功执行的时间success.form.bv
-    //语法$(表单).on('success.form.bv',fun)
-    $('.form-login').on('success.form.bv', function (e) {
-        //提交按钮有默认行为需要进行阻止
+    //表单重置
+    $('.btn-reset').click(function (e) {
+        // 当我们初始化好表单校验插件时，我们可以通过以下方法来获取表单校验的validator实例，通过validator实例调用一些方法来完成某些功能。
+        // var validator = $("#form").data('bootstrapValidator');  //获取表单校验实例
+        //恢复表单默认样式 去掉提示信息与图标
+        $(".form-login").data('bootstrapValidator').resetForm();
+    })
+
+
+    //登录按钮
+    // 注册表单验证成功事件 插件提供的一个方法表单验证成功时触发该事件
+    // 语法 $("#form").on('success.form.bv',fun)
+    $(".form-login").on('success.form.bv', function (e) {
+        // console.log(1);
+        //会刷新页面
+
+        //阻止提交按钮的默认行为 
         e.preventDefault();
-
-        //使用ajax提交数据和后台进行交互
+        //调用ajax与后台交互
         $.ajax({
             type: 'post',
             url: '/employee/employeeLogin',
-            dataType: 'json',
             data: $('.form-login').serialize(),
+            dataType: 'json',
             success: function (res) {
                 console.log(res);
-
-                // if (res.success) {
-                //     location.href = '../index.html'
-                // }
-                //后台响应结果为成功
-                res.success && (location.href = 'http://localhost:3000/admin/index.html');
-
-                //后台响应结果失败
-                // 插件提供了 updateStatus(), 可以用于改变表单状态 
-                //使用updateStatus(field, status, validatorName)方法更新字段的状态
-                //获取实例
-                //  updateStatus(数据name属性, 状态，提示信息)
-                //第一个参数
-                //第二个参数状态
-                // - INVALID ：校验失败的
-                // - VALID：校验成功的
-                // 第三个参数 validatorName 提示的数据  但是必须从已有的提示信息中选取  $('.form-login').bootstrapValidator()从这个 fields:中选取数据
-                res.error == 1000 && $('.form-login').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback');
-                res.error == 1001 && $('.form-login').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
+                res.success && (location.href = 'http://localhost:3000/admin/index.html')
             }
         })
     })
-
-
 })
